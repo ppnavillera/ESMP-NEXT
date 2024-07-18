81eee6bd99938856f8fe7b05f0f075fee5694968
pp.navillera <pp.navillera@gmail.com>
2024-07-19 02:35:45 +0900

    "Updated .gitignore and modified page.tsx"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold text-red-200 text-6xl text-center mb-8">
-        Hello!
+      <h1 className="font-bold text-red-300 text-6xl text-center mb-8">
+        ESMP !
       </h1>
       <nav className="border-gray-900 border p-4 rounded-lg shadow-lg bg-white">
         <ul className="flex justify-center space-x-4">
